afc30081adda3e5b85a764c6dd6c39f5d505d814
Piotrek <piotr.pietruszka@o2.pl>
2017-08-22 13:50:32 +0200

    perceptron

--- neural network/1.Perceptron/Perceptron/index.js
+++ neural network/1.Perceptron/Perceptron/index.js
@@ -1,2 +1,23 @@
+"use strict";
 
-module.exports = 5;
+const sign = (num)=>{
+    if(num >= 0) return 1;
+    return -1;
+};
+
+class Perceptron {
+    constructor(){
+        //set initial random weights of input data
+        this.weight = [(Math.random()*2)-1,(Math.random()*2)-1];
+    };
+
+    guess(inputs){
+        let sum = 0;
+        if(!Array.isArray(inputs)) throw new TypeError("Input argument should be an array.");
+        if(inputs.length !== 2) throw new TypeError("Algorithm expect inputs argument of size 2.");
+        inputs.map((input, index) => sum += input * this.weight[index]);
+        return sign(sum);
+    };
+};
+
+module.exports = Perceptron;
--- neural network/1.Perceptron/perceptron.spec.js
+++ neural network/1.Perceptron/perceptron.spec.js
@@ -1,10 +1,27 @@
+"use strict";
 const assert = require('assert');
+const expect = require('chai').expect;
+const Perceptron = require('./Perceptron');
+describe('Perceptron execution', function() {
+    describe('#guess()', function() {
+        it('should throw Error object if input argument isn\'t a table', function() {
+            expect(()=>{new Perceptron().guess(1)}).to.throw(TypeError, /Input argument should be an array./);
+            expect(()=>{new Perceptron().guess("1")}).to.throw(TypeError, /Input argument should be an array./);
+            expect(()=>{new Perceptron().guess([1,2])}).to.not.throw(TypeError, /Input argument should be an array./);
 
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(4));
-            assert.equal(require('./Perceptron'),6)
+        });
+        it('should throw Error when input array\'s length is not equal 2', ()=>{
+            expect(()=>{new Perceptron().guess([1,2])}).to.not.throw(TypeError, /Algorithm expect inputs argument of size 2./);
+            expect(()=>{new Perceptron().guess([1,2,3])}).to.throw(TypeError, /Algorithm expect inputs argument of size 2./);
+            expect(()=>{new Perceptron().guess([1])}).to.throw(TypeError, /Algorithm expect inputs argument of size 2./);
         });
     });
 });
+describe('Perceptron performence', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1,2,3].indexOf(4));
+        });
+    });
+});
+console.log(new Perceptron().guess([1,5]));
