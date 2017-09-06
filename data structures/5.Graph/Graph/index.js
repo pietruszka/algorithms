38e4027b7034d5ebbf7644897843043c6518906d
Piotrek <piotr.pietruszka@o2.pl>
2017-09-06 15:53:27 +0200

    graph

--- data structures/5.Graph/Graph/index.js
+++ data structures/5.Graph/Graph/index.js
@@ -1,14 +1,41 @@
 "use strict";
 
 class Graph{
-  constructor(){
-      this.verticies = [];
-      this.edges = [];
+  constructor(v){
+      this.verticies = v;
+      this.edges = 0;
+      this.adj = [];
       this.numberOfEdge = 0;
+
+      for(let i = 0; i < this.verticies; ++i){
+          this.adj[i] = [];
+          this.adj[i].push("");
+      }
   };
 
-  addVertex(vertex){
-      this.verticies.push(vertex);
-      this.edges[vertex] = [];
+  addEdge(v,w){
+      this.adj[v].push(w);
+      this.adj[w].push(v);
+      this.edges++;
+  };
+
+  showGraph(){
+      for(let i = 0; i< this.verticies; ++i){
+          //putstr(i + " -> ");
+          for(let j = 0; j < this.verticies; ++j){
+              if(this.adj[i][j] != undefined){
+                  console.log(`${i} -> ${this.adj[i][j]}`);
+                  //putstr(this.adj[i][j]+ ' ');
+              }
+          }
+          //print();
+      }
   };
 };
+
+
+let g  = new Graph(3);
+g.addEdge(0,1);
+g.addEdge(0,2);
+
+g.showGraph();
